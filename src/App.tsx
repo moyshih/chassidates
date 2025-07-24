aacbecdd7b4f7fd526426eadc0191efccb7bd654
Moshe Halperin <moyshih@gmial.com>
2025-07-24 13:55:00 +0300

    fix: replace BrowserRouter with HashRouter for improved routing stability

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,8 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
+            {/* <Route path="*" element={<NotFound />} /> */}
+            <Route path="*" element={<Index />} />
           </Routes>
         </HashRouter>
       </TooltipProvider>
